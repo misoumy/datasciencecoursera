9838bbc3063eb3abaeb21d0a331cc470c9c578fd
Mike Soumy <misoumy@users.noreply.github.com>
2015-04-26 22:41:05 +0000

    Project assignment

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# datasciencecoursera
-My repo for the Data Science series of courses at the John Hopkins university.
+Getting-and-Cleaning-Data
+=========================
+
+Getting and Cleaning Data Course Project (Coursera)
+
+Description
+-----------
+
+The purpose of this project is to demonstrate ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis.
+
+The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained:
+
+[http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+
+Here are the data for the project:
+
+[https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+
+The R script called run_analysis.R does the following. 
+
+* Merges the training and the test sets to create one data set.
+* Extracts only the measurements on the mean and standard deviation for each measurement. 
+* Uses descriptive activity names to name the activities in the data set
+* Appropriately labels the data set with descriptive variable names. 
+* From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+What you find in this repository
+--------------------------------
+
+* CodeBook.md: information about raw and tidy data set 
+* README.md: this file
+* run_analysis.R: R script to transform raw data set in a tidy one
+* tidy.txt: a tidy data set obtained following the instructions in the description of the course project. To load and check the data, you can use the read.table as described below:
+```
+    averages <- read.table("tidy.txt", header=TRUE)
+```
+
+How to
+------
+
+* clone this repository
+* download compressed raw data
+* unzip raw data and copy the directory 'UCI HAR Dataset' to the cloned repository root directory
+* open a R console and set the working directory to the repository root
+```
+    setwd("{...}")
+```
+* run run_analysis.R script 
+```
+    source("run_analysis.R")
+```
---
+++ run_analysis.R
@@ -0,0 +1,74 @@
+## run_analysis.R, 4/26/2015
+##
+## Works with the "Human Activity Recognition Using Smartphones Data Set". The script merges
+## the training and the test sets and extracts only the measurements on the mean and standard
+## deviation for each measurement.
+## The output is a tidy data set with the average of each variable for each activity and each
+## subject.
+##
+## Example:
+##  setwd("R/course/Project-C&TD/UCI HAR Dataset/")         # enter the directory with project data
+##  source("../run_analysis.R")
+##
+## Output:
+##  the tidy.txt file written in the current working directory
+##
+## Caveats:
+##  - fails when the data files are not found.
+
+
+# sanity checks
+require(data.table)
+
+trainfiles      <- c("subject_train.txt","y_train.txt","X_train.txt")
+testfiles       <- c("subject_test.txt", "y_test.txt", "X_test.txt")
+if (!(all(sapply(trainfiles, function(x) {file.exists(file.path("train", x))})) &
+    all(sapply( testfiles, function(x) {file.exists(file.path( "test", x))})))) {
+    stop("Unable to find the data files, please check your working directory.")
+}
+
+# features and the activity names
+features        <- read.table("features.txt",        col.names = c("id", "name"))
+activity        <- read.table("activity_labels.txt", col.names = c("activityID", "name"))
+
+# prepare a list of features related to mean or std
+columns         <- rep("NULL", nrow(features))
+indices         <- grep("(mean|std)\\(\\)", features$name)
+columns[indices] <- "numeric"
+
+# read and then merge the 'Measurements' data
+trainMeasures   <- read.table(file.path("train", "X_train.txt"), colClasses = columns)
+testMeasures    <- read.table(file.path("test" , "X_test.txt"),  colClasses = columns)
+measures        <- rbind(trainMeasures, testMeasures)
+rm(trainMeasures, testMeasures)
+
+# read and then merge the 'Subject' data
+trainSubjects   <- read.table(file.path("train", "subject_train.txt"))
+testSubjects    <- read.table(file.path("test",  "subject_test.txt"))
+subjects        <- rbind(trainSubjects, testSubjects)
+rm(trainSubjects, testSubjects)
+names(subjects) <- "subject"
+
+# read and then merge the 'Activity' data
+trainActivity   <- read.table(file.path("train", "y_train.txt"))
+testActivity    <- read.table(file.path("test",  "y_test.txt"))
+activities      <- rbind(trainActivity, testActivity)
+names(activities) <- "activityID"
+rm(trainActivity, testActivity)
+
+# column merge the subjects to activities and to measurements
+dataset <- cbind(subjects, activities, measures)
+rm(subjects, activities, measures)
+
+# replace integers with the 'meaningful activity names' and do the same also with the column names
+dataset$activityID <- factor(dataset$activityID, labels = activity$name)
+names(dataset)     <- c("subject", "activity", as.character(features$name[indices]))
+
+# get the averages for the 'tidy data set' requested
+dt <- data.table(dataset)
+averages <- dt[, lapply(.SD, mean), by = c("subject", "activity")]
+averages <- averages[order(averages$subject)]
+rm(dt)
+
+# write the result
+write.table(averages, file="tidy.txt", row.name=FALSE)
